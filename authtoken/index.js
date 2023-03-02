a5e9074caf2d65e7974f347a2fc7d5902b2a43ae
chinonsoScelloo <christian.onaga@scelloo.com>
2023-03-02 22:20:22 +0100

    user

--- authtoken/index.js
+++ authtoken/index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+//import Routes
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 //Connect to db
 mongoose.connect(process.env.DB_CONNECT,
@@ -10,9 +13,8 @@ mongoose.connect(process.env.DB_CONNECT,
 .then(()=> console.log('connected to DataBase'))
 .catch((err)=>{console.log("failed to connect to MongoDb",err)});
 
-//import Routes
-const authRoute = require('./routes/auth');
-
+//middleware
+app.use(express.json);
 //Routes middleware
 app.use('/api/user',authRoute);
-app.listen(3000,() => console.log("running on port 300"));
+app.listen(3000,() => console.log("running on port 3000"));
--- authtoken/routes/auth.js
+++ authtoken/routes/auth.js
@@ -1,14 +1,34 @@
 const router = require('express').Router(); //.Router();
-const User = require('../model/User')
-router.post('/register',(req,res)=>{
-//res.send('Register');
-const user = new User({
-    name:req.body.name,
-    email: req.body.email,
-    password:req.body.password,
-    username:req.body.username
+const Joi = require('@hapi/joi');
+
+//VALIDATION
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+    username: Joi.string().min(4).required()
 })
 
+const User = require('../model/User');
+router.post('/register', async (req,res)=>{
+//res.send('Register');
+//VALIDATE USER BEFORE SAVING USER
+const validation = Joi.validate(req.body);
+res.send(validation);
+// const user = new User({
+//     name:req.body.name,
+//     email: req.body.email,
+//     password:req.body.password,
+//     username:req.body.username
+// });
+// try {
+//     const savedUser = await user.save();
+//     res.send(savedUser);
+// } catch (err) {
+//  res.status(400).send(err)   
+// }
+
+// 
 });
 
 
