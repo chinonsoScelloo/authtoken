5c3d4aee96ac5ac2ce4355d28e25a746c75cb4d7
chinonsoScelloo <christian.onaga@scelloo.com>
2023-03-04 19:57:54 +0100

    get requests

--- authtoken/routes/posts.js
+++ authtoken/routes/posts.js
@@ -1,12 +1,16 @@
 const router = require('express').Router(); //.Router();
 const verify = require('./verifyToken');
+const User = require('../model/User');
+
 //add the middleware on the parameter to make it private
 router.get('/',verify,(req,res)=>{
-    res.json({
-        posts:{
-            title:"MY POST",
-            description:"DATA NOT AVAILABLE WITHOUT LOGIN"
-        }
-    })
+    // res.json({
+    //     posts:{
+    //         title:"MY POST",
+    //         description:"DATA NOT AVAILABLE WITHOUT LOGIN"
+    //     }
+    // })
+    res.send(req.user);
+   // User.findOne(req.user);
 })
 module.exports = router;
